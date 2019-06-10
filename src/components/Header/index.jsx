a172b3403952a64d37497a81a445679c6731e20b
Jiang Peiran <1070045244@qq.com>
2019-06-10 22:43:11 +0800

    basic skeleton &&  header ui

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+import logo from '@/assets/cnodejs_light.svg'
+
+import './header.scss'
+
+const Header = () => {
+  const navList = [
+    { name: '全部', value: 'all' },
+    { name: '精华', value: 'good' },
+    { name: '分享', value: 'share' },
+    { name: '问答', value: 'ask' },
+    { name: '招聘', value: 'job' },
+    { name: '关于', value: 'about' }
+  ]
+
+  const [ activeItem, setActiveItem ] = useState('all')
+
+  const selectNav = (index) => {
+    setActiveItem(navList[index].value)
+  }
+
+  return (
+    <header>
+      <div className="header-top">
+        <img src={logo} alt="logo"/>
+      </div>
+
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+          {
+            navList.map((item, index) => {
+              return (
+                <Menu.Item 
+                  name={item.name}
+                  active={item.value === activeItem}
+                  key={index}
+                  onClick={() => selectNav(index)}
+                />
+              )
+            })
+          }
+        </Menu>
+      </Segment>
+    </header>
+  ) 
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom'
+
+import 'semantic-ui-css/semantic.min.css'
+import './styles/index.scss'
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
