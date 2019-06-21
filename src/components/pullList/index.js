4d21a07270a043b358385e2774707666f0531ab1
chaser-home <1070045244@qq.com>
2019-06-21 23:17:36 +0800

    add hook

--- src/components/pullList/index.js
+++ src/components/pullList/index.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
-// import _func from 'lodash/function'
+import { useEventListener } from '@/hooks'
 
-// 上拉加载
-// 下拉刷新
 const PullList = props => {
 
   // const [timer, setTimer] = useState()
@@ -19,14 +17,7 @@ const PullList = props => {
     }, 100)
   }
 
-  useEffect(() => {
-    document.addEventListener('scroll', scrollHandler)
-
-    return () => {
-      console.log('remove')
-      document.removeEventListener('scroll', scrollHandler)
-    }
-  })
+  useEventListener('scroll', scrollHandler)
 
   return (
     <figure
