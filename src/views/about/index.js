7d68229dddc062dd647231f3f7bcd0614198d590
chaser-gosuncn <1070045244@qq.com>
2019-07-17 10:54:11 +0800

    fix json

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import aboutList from '@/assets/about.json'
+
 import './about.scss'
 
 const About = () => {
+
   return (
     <section className="about-page">
       <h3>关于此项目</h3>
       <ul>
-        <li>拥抱新特性，使用React 16 Hooks全面编写，封装多个基础Hook</li>
-        <li>自行实现多种常见交互，下拉刷新上拉加载等等</li>
-        <li>作者chaser，wechat: peiran0502</li>
+      {
+        aboutList.map((item, index) => {
+          return (
+            <li key={index}>{ item }</li>
+          )
+        })
+      }
       </ul>
     </section>
   )
