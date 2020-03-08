e982f27a3e7d76d31091e28b7eb6e2370a9f5907
impeiran <1070045244@qq.com>
2020-03-08 09:29:07 +0800

    init

--- src/store/index.js
+++
@@ -1,4 +0,0 @@
-import reducer from './reducer'
-import { createStore } from 'redux'
-
-export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
